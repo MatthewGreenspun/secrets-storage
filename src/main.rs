f38d4f88e82d8faf0b8167bab1cbb060e8844551
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-08-27 13:21:37 -0400

    improved error handling

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,74 @@
 use std::path::Path;
 
-fn main() {
+fn main(){
     let mut args = std::env::args().skip(1);
-    let command = args.next().expect("expected command");
+    let command = match args.next(){
+        Some(command) => command,
+        None => {
+            println!("\ncommands:\n\n\tsecrets-storage save <project> <file>\n\tsecrets-storage get <project> <file> <location - optional>");
+            return;
+        }
+    };
+
+    let project = match args.next(){
+        Some(project) => project,
+        None => {
+            println!("error: no project provided");
+            return;
+        },
+    };
+    let file_str = match args.next(){
+        Some(file_str) => file_str,
+        None => {
+            println!("error: no file provided");
+            return;
+        },
+    };
 
     if command == "save" {
-        //command is in form: secrets-storage save <project> <file>
-
-        let project = args.next().expect("no project provided");
-        let file_str = args.next().expect("no file provided");
-
-        let storage_path_str = format!("C:/Users/matth/secrets-storage/projects/{}/", project);
-        let storage_path = Path::new(&storage_path_str);
-        if !storage_path.exists(){
-            std::fs::create_dir_all(storage_path).expect("failed to create path to store files");
+        match save_secrets(project, file_str) {
+            Ok(_) => println!("file saved successfully"),
+            Err(_) => println!("error saving file"), 
         }
 
-        let file = Path::new(&file_str).file_name().expect("failed to retrieve file name");
-        let full_path = storage_path.join(file);
-        std::fs::copy(file_str, full_path).expect("failed to save file");
     } else if command == "get" {
-        //command is in form: secrets-storage get <project> <file> <location>
-        //moves stored file to location
+        let location_str = match args.next(){
+            Some(location) => location,
+            None => String::from("./"),
+        };
 
-        let project = args.next().expect("no project provided");
-        let file_str = args.next().expect("no file provided");
-        let location_str = args.next().expect("no location provided");
-        let storage_path_str = format!("C:/Users/matth/secrets-storage/projects/{}/{}", project, file_str);
-
-        let file = Path::new(&file_str);
-        let location = Path::new(&location_str);
-        let full_location = location.join(file);
-
-        std::fs::copy(storage_path_str, full_location).expect("failed to retrieve file");
+        match get_secrets(project, file_str, location_str) {
+            Ok(_) => println!("retrieved file successfully"),
+            Err(_) => println!("error retrieving file"), 
+        }
     }
+}
+
+fn save_secrets(project: String, file_str: String) -> std::io::Result<()>{
+    //command is in form: secrets-storage save <project> <file>
+
+    let storage_path_str = format!("C:/Users/matth/secrets-storage/projects/{}/", project);
+    let storage_path = Path::new(&storage_path_str);
+    if !storage_path.exists(){
+        std::fs::create_dir_all(storage_path)?;
+    }
+
+    let file = Path::new(&file_str).file_name().expect("failed to retrieve file name");
+    let full_path = storage_path.join(file);
+    std::fs::copy(file_str, full_path)?;
+    Ok(())
+}
+
+fn get_secrets(project: String, file_str: String, location_str: String) -> std::io::Result<()>{
+    //command is in form: secrets-storage get <project> <file> <location>
+    //moves stored file to location
+
+    let storage_path_str = format!("C:/Users/matth/secrets-storage/projects/{}/{}", project, file_str);
+
+    let file = Path::new(&file_str);
+    let location = Path::new(&location_str);
+    let full_location = location.join(file);
+
+    std::fs::copy(storage_path_str, full_location)?;
+    Ok(())
 }
