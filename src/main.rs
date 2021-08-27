f2c2e6d6b9a197c1d465c6cc37f7adc74321f176
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-08-27 10:12:01 -0400

    use path module to join paths and check if paths exist

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,28 @@ fn main() {
         let project = args.next().expect("no project provided");
         let file_str = args.next().expect("no file provided");
 
-        let path_str = format!("C:/Users/matth/secrets-storage/projects/{}/", project);
-        let path = Path::new(&path_str);
-        std::fs::create_dir_all(path).expect("failed to create path to store files");
+        let storage_path_str = format!("C:/Users/matth/secrets-storage/projects/{}/", project);
+        let storage_path = Path::new(&storage_path_str);
+        if !storage_path.exists(){
+            std::fs::create_dir_all(storage_path).expect("failed to create path to store files");
+        }
 
         let file = Path::new(&file_str).file_name().expect("failed to retrieve file name");
-        let full_path = path.join(file);
+        let full_path = storage_path.join(file);
         std::fs::copy(file_str, full_path).expect("failed to save file");
     } else if command == "get" {
         //command is in form: secrets-storage get <project> <file> <location>
+        //moves stored file to location
+
         let project = args.next().expect("no project provided");
-        let file = args.next().expect("no file provided");
-        let mut location = args.next().expect("no location provided");
-        location.push_str(&file);
-        let path = format!("./projects/{}/{}", project, file);
-        std::fs::copy(path, location).expect("failed to retrieve file");
+        let file_str = args.next().expect("no file provided");
+        let location_str = args.next().expect("no location provided");
+        let storage_path_str = format!("C:/Users/matth/secrets-storage/projects/{}/{}", project, file_str);
+
+        let file = Path::new(&file_str);
+        let location = Path::new(&location_str);
+        let full_location = location.join(file);
+
+        std::fs::copy(storage_path_str, full_location).expect("failed to retrieve file");
     }
 }
