4be2de92a930249e3643ae3404942a90dc182d6f
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-08-26 21:58:02 -0400

    use path module to allow path to file to be provided when saving

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
+use std::path::Path;
+
 fn main() {
     let mut args = std::env::args().skip(1);
     let command = args.next().expect("expected command");
 
     if command == "save" {
         //command is in form: secrets-storage save <project> <file>
+
         let project = args.next().expect("no project provided");
-        let file = args.next().expect("no file provided");
-        let path = format!("./projects/{}/{}", project, file);
-        std::fs::create_dir(format!("./projects/{}", project)).expect("failed to create path to store files");
-        std::fs::copy(file, path.clone()).expect("failed to save file");
+        let file_str = args.next().expect("no file provided");
+
+        let path_str = format!("C:/Users/matth/secrets-storage/projects/{}/", project);
+        let path = Path::new(&path_str);
+        std::fs::create_dir_all(path).expect("failed to create path to store files");
+
+        let file = Path::new(&file_str).file_name().expect("failed to retrieve file name");
+        let full_path = path.join(file);
+        std::fs::copy(file_str, full_path).expect("failed to save file");
     } else if command == "get" {
         //command is in form: secrets-storage get <project> <file> <location>
         let project = args.next().expect("no project provided");
@@ -16,6 +24,6 @@ fn main() {
         let mut location = args.next().expect("no location provided");
         location.push_str(&file);
         let path = format!("./projects/{}/{}", project, file);
-        std::fs::copy(path,location).expect("failed to retrieve file");
+        std::fs::copy(path, location).expect("failed to retrieve file");
     }
 }
