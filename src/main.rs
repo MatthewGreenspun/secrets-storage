44fa1c803fbe0a474be7971577ff7a8997fe0f16
Matthew Greenspun <78003195+MatthewGreenspun@users.noreply.github.com>
2021-08-27 15:46:51 -0400

    added ability to delete stored files and projects

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,12 @@ fn main(){
     let command = match args.next(){
         Some(command) => command,
         None => {
-            println!("\ncommands:\n\n\tsecrets-storage save <project> <file>\n\tsecrets-storage get <project> <file> <location - optional>");
+            println!("
+                \nCommands:\n
+                \n\tsecrets-storage save <project> <file>
+                \n\tsecrets-storage get <project> <file> <location - optional>
+                \n\tsecrets-storage delete <project> <file - optional>
+            ");
             return;
         }
     };
@@ -14,34 +19,34 @@ fn main(){
         let project = match args.next(){
             Some(project) => project,
             None => {
-                println!("error: no project provided\nfull command is: \n\tsecrets-storage save <project> <file>");
+                println!("Error: no project provided\nfull command is: \n\tsecrets-storage save <project> <file>");
                 return;
             },
         };
         let file_str = match args.next(){
             Some(file_str) => file_str,
             None => {
-                println!("error: no file provided\nfull command is: \n\tsecrets-storage save <project> <file>");
+                println!("Error: no file provided\nfull command is: \n\tsecrets-storage save <project> <file>");
                 return;
             },
         };
         match save_secrets(project, file_str) {
-            Ok(_) => println!("file saved successfully"),
-            Err(_) => println!("error saving file"), 
+            Ok(_) => println!("File saved successfully"),
+            Err(_) => println!("Error saving file"), 
         }
 
     } else if command == "get" {
         let project = match args.next(){
             Some(project) => project,
             None => {
-                println!("error: no project provided\nfull command is: \n\tsecrets-storage get <project> <file> <location - optional>");
+                println!("Error: no project provided\nfull command is: \n\tsecrets-storage get <project> <file> <location - optional>");
                 return;
             },
         };
         let file_str = match args.next(){
             Some(file_str) => file_str,
             None => {
-                println!("error: no file provided\nfull command is: \n\tsecrets-storage get <project> <file> <location - optional>");
+                println!("Error: no file provided\nfull command is: \n\tsecrets-storage get <project> <file> <location - optional>");
                 return;
             },
         };
@@ -52,8 +57,25 @@ fn main(){
 
         match get_secrets(project, file_str, location_str) {
             Ok(message) => println!("{}", message),
-            Err(_) => println!("error retrieving file"), 
+            Err(_) => println!("Error retrieving file"), 
         }
+    } else if command == "delete" {
+        let project = match args.next(){
+            Some(project) => project,
+            None => {
+                println!("Error: no project provided\nfull command is: \n\tsecrets-storage delete <project> <file - optional>");
+                return;
+            },
+        };
+        let file_str = match args.next(){
+            Some(file_str) => file_str,
+            None => String::new(),
+        };
+        match delete_secrets(project, file_str) {
+            Ok(message) => println!("{}", message),
+            Err(_) => println!("Error deleting file or directory"), 
+        }
+
     } else {
         println!("{} is not a valid command", command);
     }
@@ -86,14 +108,41 @@ fn get_secrets(project: String, file_str: String, location_str: String) -> std::
     
     let mut input = String::new();
     if full_location.exists() {
-        print!("file {} already exists. Would you like to replace it?[y/n] ", &full_location.to_str().unwrap());
+        print!("File {} already exists. Would you like to replace it?[y/n] ", &full_location.to_str().unwrap());
         let _  = std::io::stdout().flush();
-        std::io::stdin().read_line(&mut input).expect("failed to read user input");
+        std::io::stdin().read_line(&mut input).expect("Failed to read user input");
     }
 
     if input.trim() == "y" || !full_location.exists() {
         std::fs::copy(storage_path_str, full_location)?;
-        return Ok("retrieved file successfully");
+        return Ok("Retrieved file successfully");
     }
     Ok("")
+}
+
+fn delete_secrets(project: String, file_str: String) -> std::io::Result<&'static str> {
+    let mut input = String::new();
+    if file_str.len() > 0 {
+        let storage_path_str = format!("C:/Users/matth/secrets-storage/projects/{}/{}", project, &file_str);
+        print!("Are you sure you want to delete {}?[y/n] ", &file_str);
+        let _ = std::io::stdout().flush();
+        std::io::stdin().read_line(&mut input).expect("Failed to read user input");
+        if input.trim() == "y" {
+            std::fs::remove_file(storage_path_str)?;
+            return Ok("Successfully deleted file")
+        } else {
+            return Ok("")
+        }
+    } else {
+        let storage_path_str = format!("C:/Users/matth/secrets-storage/projects/{}/", &project);
+        print!("Are you sure you want to delete all files in your {} project?[y/n] ", &project);
+        let _ = std::io::stdout().flush();
+        std::io::stdin().read_line(&mut input).expect("Failed to read user input");
+        if input.trim() == "y" {
+            std::fs::remove_dir_all(storage_path_str)?;
+            return Ok("Successfully deleted directory")
+        } else {
+            return Ok("")
+        }
+    }
 }
